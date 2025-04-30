ed271fc5af2c1c1336e730cdab13e7a9d18e66cf
Alex Oh <odange2119@gmail.com>
2025-05-01 06:15:10 +0900

    first commit656

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://image.hm.com/assets/hm/4c/f2/4cf2d92e5896493974fc3799b12076551eb1dfd7.jpg?imwidth=1260"/>
+        <div>Conscious choice</div>
+        <div>벨티드 트윌 코트</div>
+        <div>₩99000</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      전체 상품 페이지
+      <ProductCard/>
     </div>
   )
 }
